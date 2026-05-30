2ec18105cdeb0270ea0fc8c0e8484fa421b2d118
arthurMiguel <arthurmsr23@gmail.com>
2026-05-30 12:09:53 -0300

    feat: Criação da base frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink> |
+      <RouterLink to="/times">Times</RouterLink>
+    </nav>
+  </header>
+
+  <main>
+    <RouterView />
+  </main>
 </template>
 
 <style scoped></style>
