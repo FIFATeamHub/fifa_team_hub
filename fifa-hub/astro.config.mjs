1647d263d19b11e9df99ac0d3518d071667ef230
camposs04 <julia.04.camposs@gmail.com>
2026-06-02 09:34:04 -0300

    Configura rotas para deploy correto

---
+++ fifa-hub/astro.config.mjs
@@ -0,0 +1,84 @@
+// @ts-check
+
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
+import starlightThemeRapidePlugin from "starlight-theme-rapide";
+
+export default defineConfig({
+	site: "https://fifa_team_hub.github.io",
+	base: "/fifa-hub/",
+	integrations: [
+		starlight({
+			plugins: [starlightThemeRapidePlugin()],
+			title: "Softex - Sistema de Gestão de Recursos",
+			social: [
+				{
+					icon: "github",
+					label: "GitHub",
+					href: "https://github.com/fifa_team_hub/fifa-hub",
+				},
+			],
+			customCss: ['./src/styles/custom.css'],
+			sidebar: [
+				{
+					label: "Início",
+					items: [
+						{
+							label: "Sobre o Projeto",
+							slug: "home/sobre",
+						},
+						{
+							label: "Equipe",
+							slug: "home/equipe",
+						},
+					],
+				},
+				{
+					label: "Visões de Produto e de Projeto",
+					items: [
+						{
+							label: "Contexto e Necessidades",
+							slug: "visao/cenario",
+						},
+						{
+							label: "Solução Proposta",
+							slug: "visao/solucao",
+						},
+					
+						{
+							label: "Requisitos do Sistema",
+							slug: "visao/levantamento-parcial",
+						},
+						{
+							label: "Cronograma e Entregas",
+							slug: "visao/cronograma",
+						},
+						{
+							label: "Comunicação e Colaboração",
+							slug: "visao/interacao",
+						},
+					],
+				},
+				{
+					label: "Lições Aprendidas",
+					items: [
+						{
+							label: "Unidade 1",
+							slug: "licoes/unidade-1",
+						},
+					],
+				},
+			
+				{
+					label: "Entregas",
+					items: [
+						{
+							label: "Unidade 1",
+							slug: "entregas/unidade-1",
+						},
+					],
+				},
+			],
+		}),
+	],
+});
