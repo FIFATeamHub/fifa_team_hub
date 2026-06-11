7e76cff992eca06da06695c276373006f5b86162
camposs04 <161164304+camposs04@users.noreply.github.com>
2026-06-11 08:04:54 -0300

    Add full_name field to RegisterSchema
    fix/add full_name

--- backend/app/routes/schema.py
+++ backend/app/routes/schema.py
@@ -5,6 +5,7 @@ from schema import Schema, fields, validate
 class RegisterSchema(Schema):
     email = fields.Email(required = True)
     password = fields.String(required = True, validate = validate.length(min = 8)) #Garante que a senha tem no mínimo 8 caractéres
+    full_name = fields.String(required=True)
     role = fields.String(required = True)
     selection_id = fields.String(allow_none = True) # allow_none pois pode ter o auditor sem nenhuma seleção especifíca
 
