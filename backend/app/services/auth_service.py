e855a7ea1b815521f0c49fc187c94de32b3c3c29
jvsauma <saumajoao@gmail.com>
2026-06-21 18:42:41 -0300

    fix: conserta erro da função os.getenv() do auth_service

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -12,9 +12,9 @@ def verify_password(plain_pass, hashed_pass):
 
 def create_access_token(data):
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=os.getenv().JWT_EXPIRE_MINUTES) #Define quando expira o token
+    expire = datetime.now(timezone.utc) + timedelta(minutes=os.getenv("JWT_EXPIRE_MINUTES")) #Define quando expira o token
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, os.getenv().JWT_SECRET_KEY, algorithm="HS256") #Gera o JWT (header.payload.assinatura)
 
 def decode_token(token):
-    return jwt.decode(token, os.getenv().JWT_SECRET_KEY, algorithms=["HS256"])
+    return jwt.decode(token, os.getenv("JWT_SECRET_KEY"), algorithms=["HS256"])
