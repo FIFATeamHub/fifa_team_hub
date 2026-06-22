d5856c805f7e59951f0a1a645c14e4605d10149b
Josef Wojtyla <josefwojtylabarrosdesouza@gmail.com>
2026-06-22 10:10:38 -0300

    fix(frontend): ajusta config do typescript para vue e arquivos js

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+// Ignora erros ao importar arquivos JS não tipados
+declare module '@/services/api';
+declare module './router/index.js';
