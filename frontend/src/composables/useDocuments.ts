ff06c9570852f0ec2fd63c9cde4756d24d3b2606
camposs04 <julia.04.camposs@gmail.com>
2026-07-09 11:13:45 -0300

    fix:Ajuste do cathc por erro de lint

--- frontend/src/composables/useDocuments.ts
+++ frontend/src/composables/useDocuments.ts
@@ -64,7 +64,7 @@ export function useDocuments() {
       documents.value = response.data.data
       pagination.value = response.data.pagination
 
-    } catch (err) {
+    } catch {
 
       error.value = 'Erro ao carregar documentos.'
 
