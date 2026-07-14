ef77f58b95917f0d248f3c5051fcbd223cb45587
Josef Wojtyla <josefwojtylabarrosdesouza@gmail.com>
2026-07-13 22:52:01 -0300

    chore: ajustes no ambiente de testes do frontend e script de seed

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vitest/config';
+import { fileURLToPath } from 'node:url'
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import viteConfig from './vite.config'
 
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'node', // ou 'jsdom' se for testar componentes web
-  },
-});
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude],
+      root: fileURLToPath(new URL('./', import.meta.url))
+    }
+  })
+)
